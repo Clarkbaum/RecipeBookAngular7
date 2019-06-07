1b5267e185a3fab0cdd95587fe0294c2f5b5d7f2
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2019-06-07 10:34:35 -0500

    name change

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './WarningAlert/warningAlert.component';
+import { WarningAlertComponent } from './warning-alert/warningAlert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
